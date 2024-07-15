f43e29ee066b8a97e723683272aa281f758ac6c0
reler <rodri.chocman.rr@gmail.com>
2024-07-15 16:24:45 -0400

    cambios

--- Juego-Cachipun-master/script.js
+++ Juego-Cachipun-master/script.js
@@ -4,28 +4,44 @@ function mostrarName(){
     document.querySelector("#jugador1Nombre").textContent= nombre;
 }
 
-function jugadaBot(jugada){
-    const azar =Math.floor(math.random()* 3) + 1;
-    const Jbot = document.getElementById("Jbot");
-    if(jugada ===1){
-        JUsuario.src= "img/piedra.png";
-    } else if(jugada===2){
-        JUsuario.src= "img/papel.png";
-    } else if(jugada===3){
-        JUsuario.src= "img/tijera.png";
+
+
+
+function jugadaUsuario(jugada){
+let Usuario= document.getElementById("JUsuario");
+if(jugada ==1){
+    Usuario.src= "img/piedra.png";
+} else if(jugada==2){
+    Usuario.src= "img/papel.png";
+} else if(jugada==3){
+    Usuario.src = "img/tijera.png";
+}
+
+jugadaBot();
+}
+
+function jugadaBot(){
+    let azar =Math.floor(Math.random()* 3) + 1;
+    
+    if(azar ==1){
+        document.querySelector('#Jbot').src= "img/piedra.png";
+    } else if(azar==2){
+        document.querySelector('#Jbot').src= "img/papel.png";
+    } else if(azar==3){
+        document.querySelector('#Jbot').src= "img/tijera.png";
     }
 
 }
 
 
-function jugadaUsuario(jugada){
-const JUsuario= document.getElementById("JUsuario");
-if(jugada ===1){
-    JUsuario.src= "img/piedra.png";
-} else if(jugada===2){
-    JUsuario.src= "img/papel.png";
-} else if(jugada===3){
-    JUsuario.src= "img/tijera.png";
-}
+function puntaje(u,b){
+    let empates=0;
+    if((u==1 && b==1) || (u==2 && b==2) ||(u==3 && b==3)  ){
+        empates++;
+        document.querySelector('#ptsEmpates').textContent=empates;
+        }
 
 }
+
+
+

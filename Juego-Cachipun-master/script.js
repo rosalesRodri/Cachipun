a21181efc6d6f12a94286f2c181b7b8842a9656e
reler <rodri.chocman.rr@gmail.com>
2024-07-17 12:02:50 -0400

    btnReiniciar

--- Juego-Cachipun-master/script.js
+++ Juego-Cachipun-master/script.js
@@ -1,6 +1,7 @@
 let empates=0;
 let puntosB=0;
 let puntosU=0;
+
 function mostrarName(){
     let nombre= document.querySelector("#nombreUsuario").value;
     document.querySelector("#jugador1Nombre").textContent= nombre;
@@ -46,11 +47,11 @@ function puntaje(u,b){
         document.querySelector('#ptsEmpates').textContent=empates;
         }
 
-    if(u==1 && b==2 || u==2 && b==3 ||u==3 && b==1 ){
+    if((u==1 && b==2) || (u==2 && b==3) ||(u==3 && b==1 )){
         puntosB++;
-        document.querySelector('#ptsJugador2').textContent=puntosU;
+        document.querySelector('#ptsJugador2').textContent=puntosB;
     }
-    if(u==2 && b==1 || u==3 && b==2 ||u==1 && b==3 ){
+    if((u==2 && b==1 )|| (u==3 && b==2 )||(u==1 && b==3 )){
         puntosU++;
         document.querySelector('#ptsJugador1').textContent=puntosU;
     }
@@ -58,9 +59,12 @@ function puntaje(u,b){
 }
 
 function reiniciar(){
-puntosB = 0;
-empates = 0;
-puntosU = 0;
+empates=0;
+puntosU=0;
+puntosB=0;
+    document.querySelector('#ptsEmpates').textContent=empates;
+    document.querySelector('#ptsJugador1').textContent=puntosU;
+    document.querySelector('#ptsJugador2').textContent=puntosB;
 
 }
 
